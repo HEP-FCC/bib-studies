4142e510aa4331addcf25d885ee0510b1c9e4b13
Aimilianos Koulouris <aimilianos.koulouris@cern.ch>
2025-07-23 12:03:02 +0200

    xml2json det dimensions script

---
+++ plotting/xml2json.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import dd4hep as dd4hepModule
+from ROOT import dd4hep
+from optparse import OptionParser
+import math
+import os
+
+######################################
+# option parser
+parser = OptionParser()
+parser.add_option('-d', '--detGeoFile',
+                  type=str, default='$K4GEO/FCCee/ALLEGRO/compact/ALLEGRO_o1_v03/ALLEGRO_o1_v03.xml',
+                  help='compact XML detector geometry file')
+
+(options, args) = parser.parse_args()
+
+geo_file_name = options.detGeoFile
+
+##################################################
+# Geometry part
+
+# useful doxy:
+# https://dd4hep.web.cern.ch/dd4hep/reference/classdd4hep_1_1Detector.html
+# https://dd4hep.web.cern.ch/dd4hep/reference/classdd4hep_1_1DetElement.html
+# https://dd4hep.web.cern.ch/dd4hep/reference/classdd4hep_1_1Volume.html
+# https://dd4hep.web.cern.ch/dd4hep/reference/classdd4hep_1_1Box.html
+# https://dd4hep.web.cern.ch/dd4hep/reference/DD4hep_2Detector_8h_source.html
+
+# Get detector name
+detector = geo_file_name.split("/")[-1].strip(".xml")
+# Load geometry
+det = dd4hep.Detector.getInstance()
+det.fromXML(geo_file_name)
+# Get subdetectors
+dict_sub = {}
+#subdet = det.detector("VertexBarrel")
+#subdets = det.detectors();
+subdets = det.sensitiveDetectors();
+for subdet_name in subdets:
+    print("==== name = ", subdet_name[0]) 
+    subdet = det.detector(subdet_name[0])
+    print("        ID = ", subdet.id())
+    print("        type = ", subdet.type())
+
+    # Get max dimensions for the subdetector in x,y,z (half-length form 0)
+    volume = subdet.volume()
+    box = volume.boundingBox()
+    #mySolid=volume.solid()
+    #print('dims',mySolid.dimensions())
+    #print('IsCylType()',mySolid.IsCylType())
+    #print("        dimensions = ", [box.x(), box.y(), box.z()])
+    # dimensions seem off (ask Brieuc) - for the moment multiply the max by a factor 10
+    max_z = 10*math.ceil(box.z()) #max z rounded up
+    max_r = max(10*box.x(),10*box.y())#10*math.ceil(math.sqrt(math.pow(box.x(),2)+math.pow(box.y(),2))) #max r rounded up
+    if(box.x()!=box.y()): print ('WARNING: different X/Y in the detector bounding box X:',box.x(),'Y:',box.y())
+
+    if subdet.id()>0.:
+      dict_sub[str(subdet.id())]={'subDetectorName': str(subdet_name[0]),'max_z': max_z, 'max_r': max_r}
+    # Fill a dictionary to match via ID subdetectors (togheter with its max dimensions)to the SimHits collections
+    #if subdet.id()>0.:
+    #    binning_string = str(nbins_z)+','+str(-max_z)+'.,'+str(max_z)+'.,'+str(nbins_r)+','+str(-max_r)+'.,'+str(max_r)+'.'
+    #    print("        binning = ", binning_string)
+    #    dict_sub[str(subdet.id())] ={"binning":  binning_string}
+
+import json
+with open(geo_file_name.split('/')[-1].strip('.xml')+'_DetectorDimensions.json', 'w') as fp:
+    json.dump(dict_sub, fp, indent=2)
