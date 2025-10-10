6c6218acf649a289edeeaef60656af117c2856a4
BrieucF <brieucfrancois@hotmail.com>
2025-10-10 18:44:49 +0200

    Try to fix CI #2

--- tests/full_chain_example_for_tests.sh
+++ tests/full_chain_example_for_tests.sh
@@ -13,7 +13,7 @@ fi
 
 # run the SIM-DIGI-RECO step if the files do not exist yet
 [ -f "ALLEGRO_sim.root" ] && echo "ALLEGRO_sim.root already exists, skipping the SIM step" || ddsim --enableGun --gun.distribution uniform --gun.energy "10*GeV" --gun.particle e- --numberOfEvents 10 --outputFile ALLEGRO_sim.root --random.enableEventSeed --random.seed 42 --compactFile $K4GEO/FCCee/ALLEGRO/compact/ALLEGRO_o1_v03/ALLEGRO_o1_v03.xml
-[ -f "ALLEGRO_sim_digi_reco.root" ] && echo "ALLEGRO_sim_digi_reco.root already exists, skipping the DIGI-RECO step" || k4run $FCCCONFIG/FullSim/ALLEGRO/ALLEGRO_o1_v03/run_digi_reco.py --IOSvc.Input ALLEGRO_sim.root --IOSvc.Output ALLEGRO_sim_digi_reco.root --doTopoClustering False
+[ -f "ALLEGRO_sim_digi_reco.root" ] && echo "ALLEGRO_sim_digi_reco.root already exists, skipping the DIGI-RECO step" || k4run $FCCCONFIG/FullSim/ALLEGRO/ALLEGRO_o1_v03/run_digi_reco.py --IOSvc.Input ALLEGRO_sim.root --IOSvc.Output ALLEGRO_sim_digi_reco.root --doTopoClustering False --CreateTruthLinks.OutputLevel 5
 
 # run local code to make sure it does not break
 [ -f "ALLEGRO_o1_v03_DetectorDimensions.json" ] && echo "ALLEGRO_o1_v03_DetectorDimensions.json already exists, skipping the JSON generation step" || $path_to_repo/plotting/xml2json.py -d $K4GEO//FCCee/ALLEGRO/compact/ALLEGRO_o1_v03/ALLEGRO_o1_v03.xml
